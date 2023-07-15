8677a9bfc1d9810f445804813e1fc15f33c4a3cd
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-07-15 12:34:36 -0300

    feat: added tailwind important

--- src/mfe-layout.tsx
+++ src/mfe-layout.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
 import { Layout } from './layout';
 import { BrowserRouter } from 'react-router-dom';
 import './tailwind.css';
+import { AppProps } from 'single-spa';
+import { ID_PROJECT_TO_TAILWIND_CONFIG } from '../config';
 
 const lifeCycles = singleSpaReact({
   React,
@@ -15,8 +18,19 @@ const lifeCycles = singleSpaReact({
       </BrowserRouter>
     );
   },
-  errorBoundary() {
-    return <span data-error-mfe-layout="hasError" />;
+  errorBoundary(err: Error, errInfo: React.ErrorInfo, props: AppProps) {
+    console.log('mfe-layout-report-error', JSON.stringify(err));
+    console.log('mfe-layout-report-errInfo', JSON.stringify(errInfo));
+    console.log('mfe-layout-report-props', JSON.stringify(props));
+
+    return (
+      <div
+        data-error-mfe-layout="hasError"
+        id={ID_PROJECT_TO_TAILWIND_CONFIG}
+        className="text-white bg-red-500 font-medium font-sans">
+        Erro ao carregar microfrontend
+      </div>
+    );
   },
 });
 
